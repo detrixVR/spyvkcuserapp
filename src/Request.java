70f5bbc2d00f101b5706a58b63699f0400486631
aminought <aminought@gmail.com>
2015-09-15 23:45:19 +0500

    Add resolving of user id

---
+++ src/Request.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Request {
+    public static String send(String request) throws IOException {
+        StringBuilder result = new StringBuilder();
+        URL url = new URL(request);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String line;
+        while((line = rd.readLine()) != null) {
+            result.append(line);
+        }
+        rd.close();
+        return result.toString();
+    }
+}
--- src/VKChase.java
+++ src/VKChase.java
@@ -1,5 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class VKChase {
-    public static void main(String[] args) {
-        
+    // Trying to write a simple app without patterns
+    public static void main(String[] args) throws IOException {
+        System.out.println("User link:");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String userlink = br.readLine();
+        String result = Request.send("https://api.vk.com/method/utils.resolveScreenName?screen_name=" + userlink + "&v=5.37");
+        System.out.println(result);
     }
 }
