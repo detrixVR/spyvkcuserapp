fd784358d59ec6a73405f5351f20b22a9e8b9f6f
aminought <aminought@gmail.com>
2015-10-26 22:03:59 +0500

    Add test for Logic.FilterGroupsByFollowerLike()

--- src/test/java/LogicTest.java
+++ src/test/java/LogicTest.java
@@ -1,10 +1,14 @@
 import controller.Logic;
+import model.Group;
 import model.GroupInfo;
+import model.Post;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 public class LogicTest {
     private Logic logic = new Logic();
@@ -24,5 +28,28 @@ public class LogicTest {
         }
         ArrayList<GroupInfo> actual = logic.formGroupsInfoFromSources(ids, names, screenNames);
         assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void testFilterGroupsByFollowerLike() throws Exception {
+        Group group1 = mock(Group.class);
+        Group group2 = mock(Group.class);
+        ArrayList<Post> posts = new ArrayList<>();
+        for(int i=0; i<3; i++) {
+            Post post = mock(Post.class);
+            when(post.getLikedUserIds()).thenReturn(new ArrayList<>(Arrays.<Long>asList((long) i, 2L, 3L)));
+            posts.add(post);
+        }
+        when(group1.getPosts()).thenReturn(posts);
+        when(group2.getPosts()).thenReturn(new ArrayList<>());
+        ArrayList<Group> groups = new ArrayList<>();
+        groups.add(group1);
+        groups.add(group2);
+
+        ArrayList<Group> filteredGroups = logic.filterGroupsByFollowerLike(groups, 1L);
+
+        assertEquals(1, filteredGroups.size());
+        assertEquals(1, filteredGroups.get(0).getPosts().size());
     }
 }
