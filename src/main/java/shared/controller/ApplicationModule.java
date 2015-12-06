97d653573b7b558d20386a1fb77949e09063c5ee
aminought <aminought@gmail.com>
2015-12-06 18:16:48 +0500

    Remove one redundant IoC-container

--- src/main/java/shared/controller/ApplicationModule.java
+++ src/main/java/shared/controller/ApplicationModule.java
@@ -1,18 +1,21 @@
 package shared.controller;
 
 import com.google.inject.AbstractModule;
-import com.google.inject.Guice;
-import com.google.inject.Injector;
 import com.google.inject.Singleton;
 import serverdaemon.controller.logic.AppLogicImpl;
 import serverdaemon.controller.logic.IAppLogic;
 import shared.controller.account_service.AccountServiceImpl;
 import shared.controller.account_service.IAccountService;
 import shared.controller.api_service.ApiServiceImpl;
-import shared.controller.api_service.ApiServiceModule;
 import shared.controller.api_service.IApiService;
 import shared.controller.db_service.DBServiceImpl;
 import shared.controller.db_service.IDBService;
+import shared.controller.json_service.IJsonService;
+import shared.controller.json_service.JsonServiceImpl;
+import shared.controller.link_builder.ILinkBuilder;
+import shared.controller.link_builder.LinkBuilderImpl;
+import shared.controller.request.IRequest;
+import shared.controller.request.RequestImpl;
 import webserver.controller.cookies_service.CookiesServiceImpl;
 import webserver.controller.cookies_service.ICookiesService;
 import webserver.view.templater.IPageGenerator;
@@ -21,14 +24,14 @@ import webserver.view.templater.PageGeneratorImpl;
 public class ApplicationModule extends AbstractModule {
     @Override
     protected void configure() {
+        bind(IRequest.class).to(RequestImpl.class).in(Singleton.class);
+        bind(ILinkBuilder.class).to(LinkBuilderImpl.class).in(Singleton.class);
+        bind(IJsonService.class).to(JsonServiceImpl.class).in(Singleton.class);
         bind(IAccountService.class).to(AccountServiceImpl.class).in(Singleton.class);
         bind(ICookiesService.class).to(CookiesServiceImpl.class).in(Singleton.class);
         bind(IPageGenerator.class).to(PageGeneratorImpl.class).in(Singleton.class);
         bind(IAppLogic.class).to(AppLogicImpl.class).in(Singleton.class);
         bind(IDBService.class).to(DBServiceImpl.class).in(Singleton.class);
-
-        Injector apiServiceInjector = Guice.createInjector(new ApiServiceModule());
-        IApiService apiService = apiServiceInjector.getInstance(ApiServiceImpl.class);
-        bind(IApiService.class).toInstance(apiService);
+        bind(IApiService.class).to(ApiServiceImpl.class).in(Singleton.class);
     }
 }
